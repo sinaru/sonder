6d8c917c3478c79559e6575de41b10557dd74d0f
Sinaru Gunawardena <sinarug@gmail.com>
2018-08-29 23:08:40 +0100

    don't show footer if it's empty

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,14 +1,16 @@
-<footer id="theme-footer" class="bottom-footer content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
-  </div>
+@if (is_active_sidebar('sidebar-footer') && is_active_sidebar('footer-left') && is_active_sidebar('footer-right'))
+  <footer id="theme-footer" class="bottom-footer content-info">
+    <div class="container">
+      @php dynamic_sidebar('sidebar-footer') @endphp
+    </div>
 
-  <div class="top row">
-    @if (is_active_sidebar('footer-left'))
-    <div class="left-block col-xs-12 col-sm-6"> @php dynamic_sidebar('footer-left') @endphp </div>
-    @endif
-    @if (is_active_sidebar('footer-right'))
-    <div class="right-block col-xs-12 col-sm-6"> @php dynamic_sidebar('footer-right') @endphp </div>
-    @endif
-  </div>
-</footer>
+    <div class="top row">
+      @if (is_active_sidebar('footer-left'))
+      <div class="left-block col-xs-12 col-sm-6"> @php dynamic_sidebar('footer-left') @endphp </div>
+      @endif
+      @if (is_active_sidebar('footer-right'))
+      <div class="right-block col-xs-12 col-sm-6"> @php dynamic_sidebar('footer-right') @endphp </div>
+      @endif
+    </div>
+  </footer>
+@endif
